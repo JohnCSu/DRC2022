0fcefffc93cf77b6c67492e15341a56c5f0a8ec6
John Su <johnchangsu@gmail.com>
2022-06-24 16:05:19 +1000

    added more instructions

--- lib/camera/camera.py
+++ lib/camera/camera.py
@@ -2,6 +2,9 @@
 This should import all the different functions from each task and
 Output return a tuple of sensor output
 
+This code should run all all the tasks in this folder and return a tuple of outputs 
+for the control to read:
+
 e.g.
 
 tuple(Lane detection,Classification (Take Left turn, Nothing, Turn Right), Object Detection (
