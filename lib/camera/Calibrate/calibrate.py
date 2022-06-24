aabc978389ad08e945fb3f2170a9047efaedac8c
John Su <johnchangsu@gmail.com>
2022-06-24 16:01:03 +1000

    Added folders for camera tasks

---
+++ lib/camera/Calibrate/calibrate.py
@@ -0,0 +1,10 @@
+'''
+This should import all the different functions from each task and
+Output return a tuple of sensor output
+
+e.g.
+
+tuple(Lane detection,Classification (Take Left turn, Nothing, Turn Right), Object Detection (
+
+Probably need to talk about it ...
+'''
